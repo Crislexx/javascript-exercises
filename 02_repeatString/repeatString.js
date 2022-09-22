507c48e11b404027f56437d0ae3977a7c544bb14
Cristian Manole <cris.alex.manole@gmail.com>
2022-09-22 14:47:59 +0300

    Finish exercises 1 through 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if (num == 0){
+        return '';
+    }else if(num == 1){
+        return string;
+    }else if(num < 0){
+        return 'ERROR';
+    }
 
+    let tempString = string;
+    for (let i=1; i<num; i++){
+        string+=tempString;
+    }
+    return string;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if((typeof start !="number") || (typeof end !="number")){
+        return "ERROR";
+    }
+
+    if(start < 0 || end < 0){
+        return "ERROR";
+    }
+
+    if(start > end){
+        let temp = 0;
+        temp = end;
+        end = start;
+        start = temp;
+    }
+
+    let sum=0;
+
+    for (let i=start; i<=end;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
